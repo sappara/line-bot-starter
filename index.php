6d7d57d8d0fed91aee652257d42dea926b8c52bc
saharei <saharai.co@gmail.com>
2020-03-22 23:51:15 +0900

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello World!";
+// echo "Hello World!";
+
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+// POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log($inputString);// ログを出力する
+
 ?>
